63a4fef05f52f5ea3a0031a27838f208bce774c2
Ahmed Adeyemi <aka6836@utulsa.edu>
2021-07-02 16:48:07 -0500

    new changes

--- main.cpp
+++ main.cpp
@@ -14,7 +14,11 @@ std::common_type_t<T,S> max(T a, S b)
     return a>b?a:b;
 }
 int main(){
-   auto a = max(2,2);     
+   Stack<std::pair<int, double>> a;
+   a.push({2,3.4}); a.push({4,3.5});    
+        
+    
+   std::cout << a << std::endl;
 
 
     
--- stack1.hpp
+++ stack1.hpp
@@ -14,11 +14,52 @@ class Stack{
         bool empty() const{
             return elems.empty();
         };
+        template<typename U> //implementing non-member template function 
+        friend std::ostream& operator << (std::ostream& strm, Stack<U> const& stck);
    
     private:
-        std::vector<T> elems;
+        std::vector<T> elems; 
 
 };
+
+template<typename T, typename S>
+class Stack<std::pair<T,S>>{
+
+    public:
+        void push(std::pair<T,S> const& elem){
+            elems.push_back(elem);
+        };
+        void pop(){
+            assert(!elems.empty());
+            elems.pop_back();
+        };
+        std::pair<T,S> const& top() const{
+            assert(!elems.empty());
+            return elems.back();
+        };
+        bool empty() const{
+            return elems.empty();
+        };
+
+    friend std::ostream& operator << (std::ostream& strm, Stack<std::pair<T,S>> const& stck){
+        for(auto start = stck.elems.rbegin();  start != stck.elems.rend(); start++){
+                    strm << *(start).first() << "    " <<  *(start).second() << " \n" ;
+            }
+            return strm;
+    }
+    private:
+        std::vector<std::pair<T,S>> elems; 
+
+};
+
+template<typename T>
+std::ostream& operator <<(std::ostream& strm, Stack<T> const& stck)
+{
+            for(auto start = stck.elems.rbegin();  start != stck.elems.rend(); start++){
+                    strm << *start << " \n" ;
+            }
+            return strm;
+}
 
 template<typename T>
 void Stack<T>::push(T const& elem){
@@ -34,4 +75,6 @@ T const& Stack<T>::top() const{
     assert(!elems.empty());
     return elems.back();
 }
+
+
 #endif
